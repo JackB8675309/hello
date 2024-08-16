e119094d0ea7aa8d90a37b790f8f14dcf41bdeff
JackB8675309 <130813728+JackB8675309@users.noreply.github.com>
2024-08-15 21:16:14 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jack Bettey
+ * Date: 09/14/2024
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Jack Bettey\n");
+
+    return 0;
+}
